b38b6eaab510bfc30d50d78cc4efcc6b3639ef3e
Tan <crowds.errors-0v@icloud.com>
2023-06-14 07:19:26 -0700

    quick push - readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# Free ChatGPT
+# TanGPT
 
-A fork. For fun :)
+Built around privacy. Fully open-source. Supports new larger 16K context for gpt-3.5, and 32k for those with gpt-4 access.
+Maintained by Tan.
+Please do not abuse the personal endpoint I host and provide as a proxy for testing.
--- src/components/AboutMenu/AboutMenu.tsx
+++ src/components/AboutMenu/AboutMenu.tsx
@@ -9,9 +9,7 @@ const AboutMenu = () => {
     <>
       <a
         className='flex py-2 px-2 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-        onClick={() => {
-          setIsModalOpen(true);
-        }}
+        onClick={() => setIsModalOpen(true)}
       >
         <div>
           <AboutIcon />
@@ -26,13 +24,15 @@ const AboutMenu = () => {
         >
           <div className='p-6 border-b border-gray-200 dark:border-gray-600'>
             <div className='min-w-fit text-gray-900 dark:text-gray-300 text-sm flex flex-col gap-3 leading-relaxed'>
-              <p>Forked by Tan.</p>
-
+              <p>
+                Forked and maintained by Tan. Built off work by Jing Hua, Antonio Cheong, and many others in the community. Check out Better ChatGPT upstream: 
+                <a href='https://github.com/ztjhz/BetterChatGPT' target='_blank'>https://github.com/ztjhz/BetterChatGPT</a>
+              </p>
               <p>
                 Source code for this project: <br />
                 <a href='https://github.com/TanGentleman/TanGPT' target='_blank'>https://github.com/TanGentleman/TanGPT</a>
               </p>
-              <h2 className='text-lg font-bold'>Please do not abuse the personal endpoint I host and provide for free.</h2>
+              <h2 className='text-lg font-bold'>Go easy on the personal endpoint I host and provide as a proxy for testing.</h2>
             </div>
           </div>
         </PopupModal>
@@ -40,5 +40,6 @@ const AboutMenu = () => {
     </>
   );
 };
+
 
 export default AboutMenu;
