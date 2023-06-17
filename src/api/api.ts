c8a8120c75b1de1433b4ba81dd933aea3bb911da
Tan <crowds.errors-0v@icloud.com>
2023-06-16 23:24:11 -0700

    Bugfix on api.ts (max_tokens)
    - Previous implementation allowed max_token value set by user to be overridden (tested by setting max tokens to low value, i.e 20, and asking to output a long story - limit not respected). This should now be fixed.

--- src/api/api.ts
+++ src/api/api.ts
@@ -39,7 +39,6 @@ export const getChatCompletion = async (
     body: JSON.stringify({
       messages,
       ...config,
-      max_tokens: undefined,
     }),
   });
   if (!response.ok) throw new Error(await response.text());
@@ -85,7 +84,6 @@ export const getChatCompletionStream = async (
     body: JSON.stringify({
       messages,
       ...config,
-      max_tokens: undefined,
       stream: true,
     }),
   });
